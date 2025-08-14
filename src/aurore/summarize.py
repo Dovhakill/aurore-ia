d6542392c536b0d5ade6160553b02e544589cf95
Dovhakill <s.denis@outlook.fr>
2025-08-14 12:15:58 +0200

    Update summarize.py

--- src/aurore/summarize.py
+++ src/aurore/summarize.py
@@ -71,7 +71,7 @@ Sources :
     )
     
     # MODIFICATION 3: On crée une instance du modèle et on génère le contenu
-    model = genai.GenerativeModel("gemini-1.0-pro")
+    model = genai.GenerativeModel("gemini-pro")
     
     resp = model.generate_content(
         contents=contents,
