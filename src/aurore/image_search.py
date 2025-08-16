a9c95522c8f3977cbd819fd7e0bfd0eb91a660d0
Dovhakill <s.denis@outlook.fr>
2025-08-16 12:01:16 +0200

    Update image_search.py

--- src/aurore/image_search.py
+++ src/aurore/image_search.py
@@ -1,4 +1,5 @@
-from unsplash import Unsplash
+# MODIFICATION 1 : On importe la classe 'Api' depuis 'unsplash.api'
+from unsplash.api import Api
 from .config import Settings
 import logging
 
@@ -9,18 +10,18 @@ def find_unsplash_image(query: str):
     Cherche une image pertinente sur Unsplash et renvoie ses détails.
     """
     try:
-        unsplash = Unsplash(Settings.UNSPLASH_ACCESS_KEY)
-        # On cherche des photos en orientation paysage pour un meilleur rendu
+        # MODIFICATION 2 : On utilise 'Api' au lieu de 'Unsplash'
+        unsplash = Api(access_key=Settings.UNSPLASH_ACCESS_KEY)
+        
         search_results = unsplash.search.photos(query, orientation="landscape", per_page=1)
         
-        if search_results and search_results.results:
-            photo = search_results.results[0]
+        if search_results and search_results.get("results"):
+            photo = search_results["results"][0]
             logger.info("Image trouvée sur Unsplash pour la recherche '%s'", query)
-            # On renvoie tout ce dont on a besoin pour l'affichage et l'attribution
             return {
-                "url": photo.urls.regular,
-                "author_name": photo.user.name,
-                "author_url": photo.user.links.html
+                "url": photo["urls"]["regular"],
+                "author_name": photo["user"]["name"],
+                "author_url": photo["user"]["links"]["html"]
             }
     except Exception as e:
         logger.warning("Erreur lors de la recherche d'image sur Unsplash : %s", e)
