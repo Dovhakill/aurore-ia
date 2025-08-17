be23f7a474e7d00ec27ea1b7365895b5cca99304
Dovhakill <s.denis@outlook.fr>
2025-08-17 08:47:13 +0200

    Update image_search.py

--- src/aurore/image_search.py
+++ src/aurore/image_search.py
@@ -13,19 +13,24 @@ def find_unsplash_image(query: str):
         auth = Auth(Settings.UNSPLASH_ACCESS_KEY, None, None)
         unsplash = Api(auth)
         
+        # La réponse globale est un DICTIONNAIRE
         search_results = unsplash.search.photos(query, per_page=1)
         
-        # On vérifie la présence de résultats via l'attribut .results
-        if search_results and search_results.results:
-            photo = search_results.results[0]
-            logger.info("Image trouvée sur Unsplash pour la recherche '%s'", query)
+        # On accède donc à la liste des photos avec des CROCHETS
+        if search_results and search_results.get("results"):
+            photo_list = search_results["results"]
             
-            # On accède aux données avec des points (objet) et non des crochets (dictionnaire)
-            return {
-                "url": photo.urls.regular,
-                "author_name": photo.user.name,
-                "author_url": photo.user.links.html
-            }
+            if photo_list:
+                # 'photo' est un OBJET dans la liste
+                photo = photo_list[0]
+                logger.info("Image trouvée sur Unsplash pour la recherche '%s'", query)
+                
+                # On accède donc aux détails de la photo avec des POINTS
+                return {
+                    "url": photo.urls.regular,
+                    "author_name": photo.user.name,
+                    "author_url": photo.user.links.html
+                }
     except Exception as e:
         logger.warning("Erreur lors de la recherche d'image sur Unsplash : %s", e)
     
