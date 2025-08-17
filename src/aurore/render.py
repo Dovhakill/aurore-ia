7d1e46fe67521fe84d5b45d1f207b2ec0f549deb
Dovhakill <s.denis@outlook.fr>
2025-08-17 14:28:28 +0200

    Update render.py

--- src/aurore/render.py
+++ src/aurore/render.py
@@ -3,13 +3,12 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from .utils import canonical_slug
 import locale
 
-# Essaye de définir la langue française pour les dates
 try:
     locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
 except locale.Error:
     locale.setlocale(locale.LC_TIME, '')
 
-def render_article(tpl_dir: str, title: str, body_html: str, sources: list[str], bullets=None, meta=None, dek=None, image=None):
+def render_article(tpl_dir: str, title: str, body_html: str, sources: list[str], category: str, bullets=None, meta=None, dek=None, image=None):
     env = Environment(loader=FileSystemLoader(tpl_dir), autoescape=select_autoescape(["html", "xml"]))
     tpl = env.get_template("article.html.j2")
     now = dt.datetime.now(dt.timezone.utc)
@@ -18,9 +17,10 @@ def render_article(tpl_dir: str, title: str, body_html: str, sources: list[str],
     html = tpl.render(
         title=title,
         published_iso=now.isoformat(),
-        published_human=now.strftime("%d %B %Y"), # On ajoute la date lisible
+        published_human=now.strftime("%d %B %Y"),
         body=body_html,
         sources=sources,
+        category=category or "International", # On passe la catégorie
         bullets=bullets or [],
         meta=meta or {},
         slug=slug,
